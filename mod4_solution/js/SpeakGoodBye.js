8a9a7efbc2163cf1f81ca991804cb805ede27efa
tjlatehjr <64728882+tjlatehjr@users.noreply.github.com>
2020-09-18 16:16:55 +0000

    Deleted

--- mod4_solution/js/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function(window) {
-    var helloSpeaker = {};
-    var names = window.names;;
-    var speakWord = "Hello";
-    helloSpeaker.speak = function(names) {
-        console.log(speakWord + " " + names);
-    };
-    window.helloSpeaker = helloSpeaker;
-})(window);
